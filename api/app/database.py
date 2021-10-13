290386af7c66a370d047fe29423356cc386f3099
Kingsley Torlowei <kingsleytorlowei@gmail.com>
2021-10-13 16:40:08 -0400

    database variable rename

--- api/app/database.py
+++ api/app/database.py
@@ -2,13 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-#SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 SQLALCHEMY_DATABASE_URL = "postgresql://kingsleytorloweikingsley:@localhost/rest_ml"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
 )
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-session = Session()
+SessionLocal = Session()
 
 Base = declarative_base()
